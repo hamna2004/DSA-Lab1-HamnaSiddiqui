b49ad445982277d228455ee210252b49e5bff1e5
Hamna Siddiqui <hsiddiqui.bscs24seecs@seecs.edu.pk>
2025-09-10 10:53:25 -0700

    Task 7 new

--- Task7.cpp
+++ Task7.cpp
@@ -1,43 +1,50 @@
 #include <iostream>
 #include <vector>
 using namespace std;
- 
+
 int main () {
-    
-    cout << "Enter elements of array:(-1 to exit) "<<endl;
-    
-    vector<int>arr;
+    // Input array elements until user enters -1
+    cout << "Enter elements of array (-1 to exit): " << endl;
+    vector<int> arr;
     int el;
-    cin>>el;
-    for(int i = 0 ; el!=-1 ; ++i){
+    cin >> el;
+
+    // Keep reading elements until -1 is entered
+    for (int i = 0; el != -1; ++i) {
         arr.push_back(el);
-        cin>>el;
+        cin >> el;
     }
-    if(arr.empty()){
-        cout<<"Array is empty";
+
+    // If array is empty, show message and exit
+    if (arr.empty()) {
+        cout << "Array is empty";
     }
     else {
-    cout << "Enter key: "<<endl;
-    int key;
-    cin>>key;
-    
-    vector<int>indices;
-    for(int i = 0 ; i<arr.size(); ++i) {
-        if(arr[i]==key){
-            indices.push_back(i);
+        // Ask user for the key to search
+        cout << "Enter key: " << endl;
+        int key;
+        cin >> key;
+
+        // Store indices of occurrences
+        vector<int> indices;
+
+        // Traverse array to find all indices where key occurs
+        for (int i = 0; i < arr.size(); ++i) {
+            if (arr[i] == key) {
+                indices.push_back(i);
+            }
+        }
+
+        // If no indices found
+        if (indices.empty()) {
+            cout << "Key not found";
+        }
+        else {
+            // Print all indices of key
+            cout << "Key is present at indices: ";
+            for (int i = 0; i < indices.size(); ++i) {
+                cout << indices[i] << " ";
+            }
         }
     }
-    if(indices.empty()){
-        cout<<"Key not found";
-    }
-
-    else {
-    cout<< "Key is present at indices ";
-
-    for(int i=0 ; i<indices.size(); ++i){
-        cout<<indices[i]<<" ";
-    }
-  }
 }
-}
-
